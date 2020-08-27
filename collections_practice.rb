d4c8ac91eb0c1b59cda86e0aef3bf868ef249435
Lisa Walters <campi_l@fortlewis.edu>
2020-08-27 21:53:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
 
+def sort_array_asc(array)
+array.sort
+end
+
+
+    def sort_array_desc(array)
+      array.sort do |a, b|
+        b <=> a
+      end
+    end
+
+    def sort_array_char_count(array)
+      array.sort_by{|a| a.length}
+    end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  new = array.reverse
+  new
+end
+def kesha_maker(array)
+  array.each {|word|
+word.sub!(word[2], "$")
+}
+array
+end
